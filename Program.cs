7efdd985e6e9a1f04db7365d62b23e75726ee4ee
Swapnali Sabale <swapnali.sabale@symphonysummit.com>
2022-02-23 19:13:57 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a = 5;
+Console.WriteLine(a);
+int b = 10;
+Console.WriteLine(b);
+int c = a+b;
+Console.WriteLine(c);
